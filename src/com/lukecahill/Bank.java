f98d4485ec75972caedeb4c0b9b05c8bf406630f
Luke Cahill <lmc@outlook.com>
2017-02-08 21:39:10 +0000

    Add authentication with a (unhashed) password. Customer has the option to update this.

--- src/com/lukecahill/Bank.java
+++ src/com/lukecahill/Bank.java
@@ -2,7 +2,6 @@ package com.lukecahill;
 
 import com.lukecahill.database.DBType;
 import com.lukecahill.database.DBUtil;
-import com.sun.javaws.exceptions.InvalidArgumentException;
 
 import java.sql.*;
 import java.util.Scanner;
@@ -17,18 +16,21 @@ public class Bank {
     Customer customer = new Customer();
     private static Scanner input = new Scanner(System.in);
 
-    private int bankId = 1;
-    private String name = "Lukes Bank";
+    private String bankName = "Lukes Bank";
 
     public void open() {
         boolean opened = false;
         int customerId;
+        String customerPassword;
+
         do {
             customerId = getCustomerId();
-            if (checkCustomerId(customerId)) {
+            customerPassword = getCustomerPassword();
+            if (checkCustomerId(customerId, customerPassword)) {
+                System.out.println("Welcome to " + this.bankName);
                 opened = true;
             } else {
-                System.out.println("Could not find a customer with that ID. Try again.");
+                System.out.println("Could not find a customer with those details. Try again.");
             }
         } while(!opened);
 
@@ -40,7 +42,7 @@ public class Bank {
     }
 
     private int getCustomerId() {
-        System.out.println("Enter your customer ID: ");
+        System.out.print("Enter your customer ID: ");
         int customerId;
 
         try {
@@ -53,16 +55,29 @@ public class Bank {
         return customerId;
     }
 
-    private boolean checkCustomerId(int customerId) {
+    private String getCustomerPassword() {
+        System.out.print("Enter password: ");
+        String customerPassword;
+        customerPassword = input.next();
+
+        return customerPassword;
+    }
+
+    private boolean checkCustomerId(int customerId, String customerPassword) {
 
         try(
                 Connection conn = DBUtil.getConnection(DBType.MYSQL_DB);
                 PreparedStatement pstmt = conn.prepareStatement(
-                        "SELECT COUNT(CustomerId) FROM customers WHERE CustomerId = ? LIMIT 1",
+                        "SELECT COUNT(CustomerId) " +
+                                "FROM customers " +
+                                "WHERE CustomerId = ? " +
+                                "AND CustomerPassword = ? " +
+                                "LIMIT 1",
                         ResultSet.TYPE_SCROLL_INSENSITIVE,
                         ResultSet.CONCUR_READ_ONLY);
         ) {
             pstmt.setInt(1, customerId);
+            pstmt.setString(2, customerPassword);
             ResultSet rs = pstmt.executeQuery();
 
             while(rs.next()) {
@@ -83,7 +98,7 @@ public class Bank {
         try(
                 Connection conn = DBUtil.getConnection(DBType.MYSQL_DB);
                 PreparedStatement pstmt = conn.prepareStatement(
-                        "SELECT CustomerId, CustomerName FROM customers WHERE CustomerId = ? LIMIT 1",
+                        "SELECT CustomerId, CustomerName, CustomerPassword FROM customers WHERE CustomerId = ? LIMIT 1",
                         ResultSet.TYPE_SCROLL_INSENSITIVE,
                         ResultSet.CONCUR_READ_ONLY);
         ) {
@@ -91,8 +106,9 @@ public class Bank {
             ResultSet rs = pstmt.executeQuery();
 
             while(rs.next()) {
-                customer.setName(rs.getString("CustomerName"));
+                customer.setCustomerName(rs.getString("CustomerName"));
                 customer.setCustomerId(rs.getInt("CustomerId"));
+                customer.setCustomerPassword(rs.getString("CustomerPassword"));
             }
         } catch (SQLException e) {
             DBUtil.showErrorMessage(e);
--- src/com/lukecahill/Customer.java
+++ src/com/lukecahill/Customer.java
@@ -1,5 +1,12 @@
 package com.lukecahill;
 
+import com.lukecahill.database.DBType;
+import com.lukecahill.database.DBUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 /**
@@ -9,8 +16,10 @@ import java.util.Scanner;
  */
 public class Customer {
 
-    private int CustomerId;
-    private String name;
+    private int customerId;
+    private String customerName;
+
+    private String customerPassword;
 
     private static Scanner input = new Scanner(System.in);
     private IsaAccount isaAccount;
@@ -18,35 +27,43 @@ public class Customer {
     private SavingsAccount savingsAccount;
 
     public void showCustomerOptions() {
+        System.out.println("Showing options for: " + this.getCustomerName());
         System.out.println("Choose an option:");
         System.out.println("1 - Show current account options");
         System.out.println("2 - Show ISA account options");
         System.out.println("3 - Show Savings account options");
-        System.out.println("q - Quit:");
+        System.out.println("4 - Change name");
+        System.out.println("5 - Change password");
+        System.out.println("q - Quit");
 
         String choice = input.next();
+        choice = choice.toLowerCase();
 
         switch(choice) {
             case "1":
                 if(currentAccount == null) {
-                    currentAccount = new CurrentAccount(getCustomerId());
+                    currentAccount = new CurrentAccount(this.getCustomerId());
                 }
                 currentAccount.showOptions();
                 break;
             case "2":
                 if(isaAccount == null) {
-                    isaAccount = new IsaAccount();
+                    isaAccount = new IsaAccount(this.getCustomerId());
                 }
                 isaAccount.showOptions();
                 break;
             case "3":
                 if(savingsAccount == null) {
-                    savingsAccount = new SavingsAccount();
+                    savingsAccount = new SavingsAccount(this.getCustomerId());
                 }
                 savingsAccount.showOptions();
                 break;
+            case "4":
+                this.changeName();
+            case "5":
+                this.checkChangePassword();
             case "q":
-                quitBank();
+                this.quitBank();
                 break;
             default:
                 System.out.println("That option does not exist. Please retry.");
@@ -54,22 +71,64 @@ public class Customer {
         }
     }
 
-    public int getCustomerId() {
-        return CustomerId;
-    }
-    public void setCustomerId(int customerId) {
-        CustomerId = customerId;
+    private void checkChangePassword() {
+        boolean changed = false;
+        do {
+            System.out.print("Enter your old password: ");
+            String newPassword = input.next();
+
+            if (newPassword.equalsIgnoreCase(this.getCustomerPassword()) || newPassword.isEmpty()) {
+                System.out.println("Invalid password, or password is the same as the last used.");
+                continue;
+            }
+
+            this.changePassword(newPassword);
+            changed = true;
+        } while(!changed);
     }
 
-    public String getName() {
-        return name;
+    private void changePassword(String password) {
+        try(
+                Connection conn = DBUtil.getConnection(DBType.MYSQL_DB);
+                PreparedStatement pstmt = conn.prepareStatement(
+                        "UPDATE customers SET CustomerPassword = ? WHERE CustomerId = ?",
+                        ResultSet.TYPE_SCROLL_INSENSITIVE,
+                        ResultSet.CONCUR_READ_ONLY)
+        ) {
+            pstmt.setString(1, password);
+            pstmt.setInt(2, this.customerId);
+            pstmt.execute();
+
+            this.setCustomerPassword(password);
+
+        } catch (SQLException e) {
+            DBUtil.showErrorMessage(e);
+        }
     }
-    public boolean setName(String name) {
+
+    private void changeName() {
+        String name = input.next();
+        this.setCustomerName(name);
+
+        // will then need to save this in the database
+    }
+
+    public int getCustomerId() {
+        return this.customerId;
+    }
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerName() {
+        return this.customerName;
+    }
+    public boolean setCustomerName(String name) {
         if(name.isEmpty() || name.equalsIgnoreCase("")) {
             return false;
         }
 
-        this.name = name;
+        this.customerName = name;
         return true;
     }
 
@@ -77,4 +136,10 @@ public class Customer {
         System.exit(0);
     }
 
+    public void setCustomerPassword(String customerPassword) {
+        this.customerPassword = customerPassword;
+    }
+    public String getCustomerPassword() {
+        return customerPassword;
+    }
 }
