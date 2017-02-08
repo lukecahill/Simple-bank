b49654ccf64553d3b9e8b72e5ce802b1ba9e4e36
Luke Cahill <lmc@outlook.com>
2017-02-08 22:28:41 +0000

    Add encrypt password class

---
+++ src/com/lukecahill/EncryptPasswords.java
@@ -0,0 +1,38 @@
+package com.lukecahill;
+
+import sun.misc.BASE64Encoder;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ *
+ * Created by Luke on 08/02/2017.
+ *
+ */
+public class EncryptPasswords {
+
+    public EncryptPasswords() {}
+
+    public String encryptPassword(String customerPassword) {
+        MessageDigest md = null;
+        try {
+            md = MessageDigest.getInstance("SHA");
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println("Could not verify password.");
+        }
+
+        try {
+            md.update(customerPassword.getBytes("UTF-8"));
+        } catch (UnsupportedEncodingException e) {
+            System.out.println("Could not verify password.");
+        }
+
+        byte raw[] = md.digest();
+        customerPassword = (new BASE64Encoder()).encode(raw);
+
+        System.out.println(customerPassword);
+        return customerPassword;
+    }
+}
